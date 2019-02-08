d8b46fd18514701f0c1533c07199761e4f773705
hishamtj <47287070+hishamtj@users.noreply.github.com>
2019-02-08 02:13:04 +0200

    Add files via upload

--- autocomplete-v1.0.js
+++ autocomplete-v1.0.js
@@ -4,10 +4,12 @@
 //***********************************************************//
 $.fn.autocomplete = function (options) {
     this.wrap("<div class='searchHolder'></div>");
+    var excludedKeyCodes = [9, 13, 16, 17, 18, 20, 27, 33, 34, 35, 36, 37, 38, 39, 40, 91, 93];
     var settings = $.extend({
         dataSource: "sample.json",
         dataSourceAllowCache: false,
         dataParameterName: "",
+        dataSourceSearchFields: "name",
         minimumCharacters: 3,
         resultsHolder: ".searchHolder",
         noResultsMessage: "No results",
@@ -16,17 +18,20 @@ $.fn.autocomplete = function (options) {
         messageHolder: null,
         showLoader: true,
         loaderHolder: ".searchHolder",
-        resultJsonTextField: "name",
-        resultJsonValueField: "id",
+        resultLinkTextFields: "name",
+        resultLinkDataAttributes: "id,name",
         resultClickFunctionName: "customClickFunction"
     }, options);
     return this.keyup(function (e) {
-        if (e.keyCode != 38 && e.keyCode != 40 && e.keyCode != 13 && e.keyCode != 39 && e.keyCode != 37) {
+        if (excludedKeyCodes.indexOf(e.keyCode) < 0) {
             if (settings.dataSource && settings.dataSource.length > 0) {
-                if (settings.resultJsonTextField && settings.resultJsonTextField.length > 0 && settings.resultJsonValueField && settings.resultJsonValueField.length > 0) {
+                if (settings.resultLinkTextFields && settings.resultLinkTextFields.length > 0 && settings.resultLinkDataAttributes && settings.resultLinkDataAttributes.length > 0) {
                     var value = $(this).val();
                     if (!$(settings.resultsHolder)) {
                         settings.resultsHolder = $(this).parent();
+                    }
+                    if (!settings.messageHolder) {                                            
+                        settings.messageHolder = settings.resultsHolder;
                     }
                     if (value.length >= settings.minimumCharacters) {
                         if (settings.showLoader) {
@@ -38,17 +43,37 @@ $.fn.autocomplete = function (options) {
                         }
                         $.ajaxSetup({ cache: settings.dataSourceAllowCache });
                         if (settings.dataSource.toLowerCase().indexOf(".json") > -1) {
-                            $.ajax({
-                                url: settings.dataSource,
-                                dataType: "json",
-                                success: function (data) {
-                                    var filteredJson = $(data).filter(function (i, n) { return n[settings.resultJsonTextField].indexOf(value) > -1 });
-                                    loadResults(filteredJson, settings);
-                                },
-                                error: function (e) {
-                                    throwError(e.responseText, settings, "error");
-                                }
-                            });
+                            if (settings.dataSourceSearchFields && settings.dataSourceSearchFields.length > 0) {
+                                $.ajax({
+                                    url: settings.dataSource,
+                                    dataType: "json",
+                                    success: function (data) {
+                                        if (settings.dataSourceSearchFields.indexOf(",") > -1) {
+                                            var SearchFields = settings.dataSourceSearchFields.split(",");
+                                            var filteredJson = [];
+                                            $.each(data, function (index, valueObj) {
+                                                for (var i = 0; i < SearchFields.length; i++) {
+                                                    if (valueObj[SearchFields[i]].toString().toLowerCase().indexOf(value) > -1) {
+                                                        filteredJson.push(data[index]);
+                                                    }
+                                                }
+                                            });
+                                            var uniqueFilteredJson = filteredJson.filter((elem, pos, arr) => arr.indexOf(elem) == pos);
+                                            loadResults(uniqueFilteredJson, settings);
+                                        }
+                                        else {
+                                            var filteredJson = $(data).filter(function (i, n) { return n[settings.dataSourceSearchFields].toString().toLowerCase().indexOf(value) > -1 });
+                                            loadResults(filteredJson, settings);
+                                        }
+                                    },
+                                    error: function (e) {
+                                        throwError(e.responseText, settings, "error");
+                                    }
+                                });
+                            }
+                            else {
+                                alert("No JSON search field name is defined");
+                            }
                         }
                         else {
                             if (settings.dataParameterName && settings.dataParameterName.length > 0) {
@@ -73,6 +98,7 @@ $.fn.autocomplete = function (options) {
                     }
                     else {
                         $(settings.resultsHolder).find(".resultsList").remove();
+                        if (settings.messageHolder) { $(settings.messageHolder).find("span.message").remove(); }
                     }
                 }
                 else {
@@ -107,18 +133,43 @@ $.fn.autocomplete = function (options) {
             if (e.keyCode == 13) { //enter
                 $(this).parent().find(".resultsList li.selected a").click();
             }
+            if (e.keyCode == 27) { //esc
+                $(this).parent().find(".resultsList").remove();
+                $(this).val("");
+            }
         }
     });
 };
 function loadResults(filteredJson, settings) {
     $(settings.resultsHolder).find(".resultsList").remove();
-    if (filteredJson != null && filteredJson.length > 0) {
+    if (filteredJson !== null && filteredJson.length > 0) {
         var list = $(settings.resultsHolder).append("<ul class='resultsList'></ul>").find("ul");
         $.each(filteredJson, function (idx, obj) {
-            var linkText, linkValue;
-            linkText = obj[settings.resultJsonTextField];
-            linkValue = obj[settings.resultJsonValueField];
-            list.append("<li><a data-value='" + linkValue + "'>" + linkText + "</a></li>");
+            var linkText, linkDataAttribute;
+            if (settings.resultLinkTextFields.indexOf(",") > -1) {
+                linkText = "";
+                var TextFields = settings.resultLinkTextFields.split(",");
+                for (i = 0; i < TextFields.length; i++) {
+                    linkText += (linkText.length > 0 ? " " : "") + obj[TextFields[i]];
+                }
+            }
+            else {
+                linkText = obj[settings.resultLinkTextFields];
+            }
+            var anchorHolder = document.createElement("li");
+            var anchor = document.createElement("a");            
+            $(anchor).html(linkText);
+            if (settings.resultLinkDataAttributes.indexOf(",") > -1) {
+                var DataAttributeFields = settings.resultLinkDataAttributes.split(",");
+                for (i = 0; i < DataAttributeFields.length; i++) {
+                    $(anchor).attr("data-" + DataAttributeFields[i], obj[DataAttributeFields[i]]);
+                }
+            }
+            else {
+                $(anchor).attr("data-" + settings.resultLinkDataAttributes, obj[settings.resultLinkDataAttributes]);
+            }
+            $(anchorHolder).append($(anchor));
+            list.append(anchorHolder);
         });
         $(settings.resultsHolder).find(".resultsList li a").click(function () {
             if (!settings.resultClickFunctionName) {
@@ -132,18 +183,22 @@ function loadResults(filteredJson, settings) {
             $(this).siblings().removeClass("selected");
             $(this).addClass("selected");
         });
+        if (settings.messageHolder) { $(settings.messageHolder).find("span.message").remove(); }
     }
     else {
-        throwError(settings.noResultsMessage, settings, "noresults");
+        $(settings.resultsHolder).find(".resultsList").remove();
+        throwError(settings.noResultsMessage, settings, "noresults");        
     }
     if (settings.showLoader) { $(settings.loaderHolder).find(".loader").remove(); }
 }
-function throwError(errorMessage, settings, errorType) {
+function throwError(errorMessage, settings, errorType) {    
     if (settings.messageAlert) {
         alert(errorMessage);
     }
-    else if (settings.messageHolder) {
-        $(settings.messageHolder).html(errorMessage);
-        $(settings.messageHolder).addClass("message " + (errorType == "error" ? "error" : "noResults"));
+    else {
+        var messageHolder = document.createElement("span");
+        $(settings.messageHolder).append(messageHolder);
+        $(settings.messageHolder).find("span").html(errorMessage);
+        $(settings.messageHolder).find("span").addClass("message " + (errorType == "error" ? "error" : "noResults"));
     }
 }
